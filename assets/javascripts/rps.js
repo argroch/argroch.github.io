e95cd82096a8a56cae95dee3cb3d1d6830e6928b
Aaron Groch <ar.groch@gmail.com>
2020-12-14 11:32:25 -0800

    redesign of landing page, and modals and links for various projects

--- assets/javascripts/rps.js
+++ assets/javascripts/rps.js
@@ -1,56 +1,3 @@
-// var userChoice = prompt("Do you choose rock, paper or scissors?");
-
-// var computerChoice = Math.random();
-// if (computerChoice < 0.34) {
-//     computerChoice = "rock";
-// } else if(computerChoice <= 0.67) {
-//     computerChoice = "paper";
-// } else {
-//     computerChoice = "scissors";
-// }
-// var compare = function(userChoice, computerChoice) {
-//     if (userChoice === computerChoice) {
-//         alert("The result is a tie!");
-//     }
-//     if (userChoice === "rock") {
-//         if (computerChoice === "scissors") {
-//             alert("Rock smashes scissors. You win!");
-//         } else {
-//             alert("Paper covers rock. You lose.");
-//         }
-//     }
-//     if (userChoice === "paper") {
-//         if (computerChoice === "rock") {
-//             alert("Paper covers rock. You win!");
-//         } else {
-//             alert("Scissors cut paper. You lose.");
-//         }
-//     }
-//      if (userChoice=== "scissors") {
-//         if (computerChoice === "rock") {
-//             alert("Hulk smashes scissors! I mean...rock smashes scissors. You lose.");
-//         } else {
-//             alert("Scissor cut paper. You win!");
-//         }
-//     }
-// };
-// compare(userChoice, computerChoice);
-// $('#rock').hover(function(){
-// 	$('#rock-text').removeClass('invisible');
-// }, function() {
-// 	$('#rock-text').addClass('invisible');
-// });
-// $('#paper').hover(function(){
-// 	$('#paper-text').removeClass('invisible');
-// }, function() {
-// 	$('#paper-text').addClass('invisible');
-// });
-// $('#scissors').hover(function(){
-// 	$('#scissor-text').removeClass('invisible');
-// }, function() {
-// 	$('#scissor-text').addClass('invisible')
-// });
-
 
 function compChoice() {
 	var computerChoice = Math.random();
@@ -63,46 +10,48 @@ function compChoice() {
 	}
 }
 
+$('#result_card').hide();
+
 $('#rock').click(function(){
 	var userChoice = "rock"
 	var computerChoice = compChoice();
-
+	$('#result_card').show();
 	if(computerChoice === "rock"){
-		alert("It's a tie.");
+		$('#result_msg').html("It's a tie.");
 	} else if(computerChoice === "paper"){
-		alert("Paper covers rock. You lose.");
+		$('#result_msg').html("Paper covers rock. You lose.");
 	} else {
-		alert("Rock crushes scissors. You win!");
+		$('#result_msg').html("Rock crushes scissors. You win!");
 	}
 });
 
 $('#paper').click(function(){
 	var userChoice = "paper"
 	var computerChoice = compChoice();
-
-	$('#result-well').removeClass('invisible');
+	$('#result_card').show();
 
 	if(computerChoice == "paper"){
 		// alert("It's a tie.");
-		$('#result').html("It's a tie.");
+		$('#result_msg').html("It's a tie.");
 	} else if(computerChoice == "rock"){
 		// alert("Paper covers rock. You win!");
-		$('#result').html("Paper covers rock. You win!");
+		$('#result_msg').html("Paper covers rock. You win!");
 	} else {
 		// alert("Scissors cut paper. You lose.");
-		$('#result').html("Scissors cut paper. You lose.");
+		$('#result_msg').html("Scissors cut paper. You lose.");
 	}
 });
 
 $('#scissors').click(function(){
 	var userChoice = "scissors"
 	var computerChoice = compChoice();
+	$('#result_card').show();
 
 	if(computerChoice == "scissors"){
-		alert("It's a tie.");
+		$('#result_msg').html("It's a tie.");
 	} else if(computerChoice == "paper"){
-		alert("Scissors cut paper. You win!");
+		$('#result_msg').html("Scissors cut paper. You win!");
 	} else {
-		alert("Rock crushes scissors. You lose.");
+		$('#result_msg').html("Rock crushes scissors. You lose.");
 	}
 });
